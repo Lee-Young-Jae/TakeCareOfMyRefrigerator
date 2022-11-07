1e1bb8d39987aafcf58926f50f1f3116fcba39ef
Lee-Young-Jae <yjlee1937@naver.com>
2022-11-07 13:24:28 +0900

    feat: [Style.theme] Common Style 작성

---
+++ frontend/src/Style/theme/index.js
@@ -0,0 +1,14 @@
+const palette = {
+  DEEP_BLUE: "#0C58EB",
+  BRIGHT_BLUE: "#0C99F5",
+  TEAL: "#01BFDD",
+  EMERALD: "#0CF5D4",
+  LIME: "#0CF5A3",
+  GRAY: "#E5E5E5",
+};
+
+const theme = {
+  palette,
+};
+
+export default theme;
