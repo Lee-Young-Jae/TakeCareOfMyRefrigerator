362a1ed3cfc7ddfba465ae2111f5550dee3deba3
Lee-Young-Jae <yjlee1937@naver.com>
2022-11-08 12:55:49 +0900

    feat: [PageCommonStyle.js] 페이지 공통 레이아웃 추가

---
+++ frontend/src/Components/layout/PageCommonStyle.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const PageCommonStyle = styled.div`
+  flex-direction: column;
+  align-items: center;
+  background: #ffffff;
+  font-weight: 600;
+  padding-top: 40%;
+  margin: 1rem 5rem;
+`;
+
+export default PageCommonStyle;
