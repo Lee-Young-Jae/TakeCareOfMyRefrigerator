92912d17acaadada044afae42fdad4ac831225d3
Lee-Young-Jae <yjlee1937@naver.com>
2022-11-08 12:55:06 +0900

    feat: [LabelInput.js] 컴포넌트 slideUp 애니메이션 추가

--- frontend/src/Components/Patterns/LabeledInput.js
+++ frontend/src/Components/Patterns/LabeledInput.js
@@ -1,12 +1,37 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css, keyframes } from "styled-components";
 import TextInput from "../common/TextInput";
+
+const slideUp = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(100%);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0%);
+  }
+`;
+
+const animationStyle = css`
+  ${({ animation }) => {
+    return (
+      animation &&
+      css`
+        animation: ${slideUp} 1s forwards ease-out;
+      `
+    );
+  }}
+`;
 
 const LabeledInputStyle = styled.div`
   display: flex;
   align-items: center;
   position: relative;
   cursor: pointer;
+
+  /** 애니메이션 */
+  ${animationStyle}
 `;
 
 const LabelStyle = styled.label`
@@ -18,6 +43,7 @@ const LabelStyle = styled.label`
   border-radius: 10px 10px 0px 0px;
   font-weight: 600;
   font-size: 0.8rem;
+  z-index: 1;
 `;
 
 const InputStyle = styled(TextInput)`
@@ -26,16 +52,33 @@ const InputStyle = styled(TextInput)`
   }
 `;
 
-const LabeledInput = ({ labelText, inputName, inputType, ...rest }) => {
+const ChildrenStyle = styled.div`
+  position: absolute;
+  right: 0;
+`;
+
+const LabeledInput = ({
+  labelText,
+  inputName,
+  inputType,
+  children,
+  autoComplete,
+  slideup,
+  animation,
+  ...rest
+}) => {
   return (
-    <LabeledInputStyle>
+    <LabeledInputStyle animation={animation}>
       <LabelStyle htmlFor={inputName}>{labelText}</LabelStyle>
       <InputStyle
         id={inputName}
         type={inputType}
+        autoComplete={autoComplete}
+        autocomplete={autoComplete}
         fullWidth
         {...rest}
       ></InputStyle>
+      <ChildrenStyle>{children}</ChildrenStyle>
     </LabeledInputStyle>
   );
 };
