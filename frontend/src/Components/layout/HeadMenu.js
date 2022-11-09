50ccc7095420cc2197ec0b6b80a5df09555f12ba
Lee-Young-Jae <yjlee1937@naver.com>
2022-11-09 10:32:20 +0900

    design: [HeadMenu.js] UI 디자인 변경

--- frontend/src/Components/layout/HeadMenu.js
+++ frontend/src/Components/layout/HeadMenu.js
@@ -3,18 +3,19 @@ import styled from "styled-components";
 
 const HeadMenuStyle = styled.h1`
   position: fixed;
-
   top: 20;
   display: flex;
   justify-content: center;
   align-items: center;
   width: 500px;
-  height: 80px;
+  height: 5rem;
   background: #ffffff;
   box-shadow: 0px 1px 0px #00000014;
   border-radius: 10px 10px 0px 0px;
   font-weight: 600;
   font-size: 1.2rem;
+  z-index: 1;
+  backdrop-filter: blur(5px);
 `;
 
 const HeadMenu = ({ children }) => {
