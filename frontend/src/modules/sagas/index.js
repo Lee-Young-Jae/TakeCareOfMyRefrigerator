7d4b4fd5e6248e3f21d837592d54e89abd882b47
Lee-Young-Jae <yjlee1937@naver.com>
2022-11-22 14:39:55 +0900

    feat: Redux-Saga 추가

---
+++ frontend/src/modules/sagas/index.js
@@ -0,0 +1,15 @@
+import { all, fork } from "redux-saga/effects";
+import userSaga from "./user";
+
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:3070";
+// axios.defaults.baseURL = "http://10.20.13.213:3065";
+axios.defaults.withCredentials = true; //사가에서 보내는 axios 요청들엔 withCredentials가 true로 공통적으로 들어간다. 서버와 쿠키 전달을 위함
+
+// ============ 이벤트 등록 ============
+export default function* rootSaga() {
+  //비동기 액션 추가.
+  yield all([fork(userSaga)]);
+}
+// ====================================
