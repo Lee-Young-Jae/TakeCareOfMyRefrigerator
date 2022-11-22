4fafd143df521b29c727c312b4d8be2826e78ca6
Lee-Young-Jae <yjlee1937@naver.com>
2022-11-22 14:39:15 +0900

    feat: Redux 추가

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./modules/reducers";
+import { applyMiddleware, createStore } from "redux"; // ,compose 배포시 composeWithDevTools 대신 compose 사용
+import { Provider } from "react-redux";
+import myLogger from "./middlewares/configureStore";
+import rootSaga from "./modules/sagas";
+import createSagaMiddleware from "redux-saga";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware, myLogger))
+  // compose(applyMiddleware(sagaMiddleware, myLogger))
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+sagaMiddleware.run(rootSaga);
+
+const rootNode = document.getElementById("root");
+ReactDOM.createRoot(rootNode).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
