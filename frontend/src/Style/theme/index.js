026853d6462990f170b9ec92e52bb587ef51d702
Lee-Young-Jae <yjlee1937@naver.com>
2022-11-26 14:19:30 +0900

    feat: 유효기간 색상 상수 추가

--- frontend/src/Style/theme/index.js
+++ frontend/src/Style/theme/index.js
@@ -1,14 +1,20 @@
-const palette = {
+const palette = Object.freeze({
   DEEP_BLUE: "#0C58EB",
   BRIGHT_BLUE: "#0C99F5",
   TEAL: "#01BFDD",
   EMERALD: "#0CF5D4",
   LIME: "#0CF5A3",
   GRAY: "#E5E5E5",
-};
+});
+const expiration = Object.freeze({
+  RED: "#FF0000",
+  ORANGE: "#FFA500",
+  TEAL: "#01BFDD",
+});
 
 const theme = {
   palette,
+  expiration,
 };
 
 export default theme;
