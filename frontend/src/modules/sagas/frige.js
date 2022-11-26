87c29f171f03d5f6dd99b2621019cb95334d893d
Lee-Young-Jae <yjlee1937@naver.com>
2022-11-27 03:13:12 +0900

    feat: 냉장고 saga 추가

--- frontend/src/modules/sagas/frige.js
+++ frontend/src/modules/sagas/frige.js
@@ -11,6 +11,9 @@ import {
   LOAD_FRIGE_FAILURE,
   LOAD_FRIGE_REQUEST,
   LOAD_FRIGE_SUCCESS,
+  LOAD_RECIPE_FAILURE,
+  LOAD_RECIPE_REQUEST,
+  LOAD_RECIPE_SUCCESS,
 } from "../reducers/frige";
 
 function loadFrigeAPI(data) {
@@ -72,6 +75,26 @@ function* deleteFrige(action) {
   }
 }
 
+function loadRecipeAPI(data) {
+  return axios.get(`/frige/recipe/list`);
+}
+
+function* loadRecipe(action) {
+  try {
+    const result = yield call(loadRecipeAPI, action.data);
+
+    yield put({
+      type: LOAD_RECIPE_SUCCESS,
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: LOAD_RECIPE_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+
 function* watchLoadFrige() {
   yield takeLatest(LOAD_FRIGE_REQUEST, loadFrige);
 }
@@ -84,6 +107,15 @@ function* watchDeleteFrige() {
   yield takeLatest(DELETE_FRIGE_REQUEST, deleteFrige);
 }
 
+function* watchLoadRecipe() {
+  yield takeLatest(LOAD_RECIPE_REQUEST, loadRecipe);
+}
+
 export default function* userSaga() {
-  yield all([fork(watchLoadFrige), fork(watchAddFrige), fork(watchDeleteFrige)]);
+  yield all([
+    fork(watchLoadFrige),
+    fork(watchAddFrige),
+    fork(watchDeleteFrige),
+    fork(watchLoadRecipe),
+  ]);
 }
