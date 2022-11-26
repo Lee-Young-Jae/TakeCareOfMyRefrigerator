6cae4d679f1f00fa671c92990a1f8502ceabc40e
Lee-Young-Jae <yjlee1937@naver.com>
2022-11-26 14:17:52 +0900

    feat: frige Saga 추가

---
+++ frontend/src/modules/sagas/frige.js
@@ -0,0 +1,89 @@
+import { all, fork, takeLatest, put, call } from "redux-saga/effects";
+import axios from "axios";
+import {} from "../reducers/shopping";
+import {
+  ADD_FRIGE_FAILURE,
+  ADD_FRIGE_REQUEST,
+  ADD_FRIGE_SUCCESS,
+  DELETE_FRIGE_FAILURE,
+  DELETE_FRIGE_REQUEST,
+  DELETE_FRIGE_SUCCESS,
+  LOAD_FRIGE_FAILURE,
+  LOAD_FRIGE_REQUEST,
+  LOAD_FRIGE_SUCCESS,
+} from "../reducers/frige";
+
+function loadFrigeAPI(data) {
+  return axios.get(`/frige/list`);
+}
+
+function* loadFrige(action) {
+  try {
+    const result = yield call(loadFrigeAPI, action.data);
+
+    yield put({
+      type: LOAD_FRIGE_SUCCESS,
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: LOAD_FRIGE_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+
+function addFrigeAPI(data) {
+  return axios.post(`/frige/list/`, data);
+}
+
+function* addFrige(action) {
+  try {
+    const result = yield call(addFrigeAPI, action.data);
+    yield put({
+      type: ADD_FRIGE_SUCCESS,
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: ADD_FRIGE_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+
+function deleteFrigeAPI(data) {
+  return axios.delete(`/frige/list/${data}`);
+}
+
+function* deleteFrige(action) {
+  try {
+    const result = yield call(deleteFrigeAPI, action.data);
+
+    yield put({
+      type: DELETE_FRIGE_SUCCESS,
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: DELETE_FRIGE_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+
+function* watchLoadFrige() {
+  yield takeLatest(LOAD_FRIGE_REQUEST, loadFrige);
+}
+
+function* watchAddFrige() {
+  yield takeLatest(ADD_FRIGE_REQUEST, addFrige);
+}
+
+function* watchDeleteFrige() {
+  yield takeLatest(DELETE_FRIGE_REQUEST, deleteFrige);
+}
+
+export default function* userSaga() {
+  yield all([fork(watchLoadFrige), fork(watchAddFrige), fork(watchDeleteFrige)]);
+}
--- frontend/src/modules/sagas/index.js
+++ frontend/src/modules/sagas/index.js
@@ -1,6 +1,7 @@
 import { all, fork } from "redux-saga/effects";
 import userSaga from "./user";
 import shoppingSaga from "./shopping";
+import frigeSaga from "./frige";
 
 import axios from "axios";
 
@@ -11,6 +12,6 @@ axios.defaults.withCredentials = true; //사가에서 보내는 axios 요청들�
 // ============ 이벤트 등록 ============
 export default function* rootSaga() {
   //비동기 액션 추가.
-  yield all([fork(userSaga), fork(shoppingSaga)]);
+  yield all([fork(userSaga), fork(shoppingSaga), fork(frigeSaga)]);
 }
 // ====================================
