d47cd3bc076d43de7334102f156c40282829962d
Lee-Young-Jae <yjlee1937@naver.com>
2022-11-28 13:23:31 +0900

    feat: 공용 애니메이션 생성

---
+++ frontend/src/Style/keyframes/loading.js
@@ -0,0 +1,11 @@
+import { keyframes } from "styled-components";
+
+//Loading Animation
+export const loading = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
